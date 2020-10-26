93287ce5c2e5c9320d55a7c015e32c05eb5f56a0
Supanut Laddayam <ynotmela@gmail.com>
2020-10-26 14:27:21 +0700

    add users

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 
+const users = [
+    {
+        name: "nut",
+        age: 23
+    },
+    {  
+        name: "keaw",
+        age: 22
+    }
+]
+
 app.get("/", (req, res, next) => {
     res.send('test heroku')
+})
+
+app.get("/users", (req, res, next) => {
+    res.json({
+        users
+    })
 })
 
 app.listen(PORT, () => {
