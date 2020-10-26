9bde717014041ec3e1d3954b9b029623f7e200dd
Supanut Laddayam <ynotmela@gmail.com>
2020-10-26 15:05:46 +0700

    add index

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 3000
 
 
@@ -14,8 +15,11 @@ const users = [
     }
 ]
 
+app.set('views', path.join(__dirname, 'views'))
+
 app.get("/", (req, res, next) => {
-    res.send('test heroku')
+    console.log(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 app.get("/users", (req, res, next) => {
